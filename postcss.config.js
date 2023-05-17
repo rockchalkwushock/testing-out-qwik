d71adc187b3c9e76a3a332a994b8e3cc0e412469
Cody Brunner <me@codybrunner.com>
2023-05-17 09:33:59 -0500

    feat(root): adds other integrations

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true,
+      },
+    },
+    cssnano: {
+      preset: 'default',
+    },
+  },
+}
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,10 +1,15 @@
 import { component$, useSignal } from '@builder.io/qwik'
+import { LuPlusCircle } from '@qwikest/icons/lucide'
 
 export const Counter = component$(() => {
   const count = useSignal(0)
   return (
-    <button class="counter" type="button" onClick$={() => count.value++}>
-      Increment {count.value}
+    <button
+      class="bg-blue-500/20 border border-blue-500 flex items-center px-4 py-1 rounded-lg space-x-2"
+      type="button"
+      onClick$={() => count.value++}
+    >
+      <LuPlusCircle /> <span>{count.value}</span>
     </button>
   )
 })
